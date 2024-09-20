b4de6c31f3a0538a064c98c8fa7b815bf9b2f823
Hadeed1009 <hadeednaeem10@gmail.com>
2024-09-20 20:52:20 +0500

    add tab bar

---
+++ src/navigation/Tab_Bar.tsx
@@ -0,0 +1,44 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import React from 'react';
+import {Home, Meal, Profile, Shopping} from '../screens';
+import {
+  CustomTabBarButton,
+  TabBarIcon,
+} from '../components/Custom_TabBar_Button';
+
+const Tab = createBottomTabNavigator();
+
+const TabBar = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarButton: props => <CustomTabBarButton {...props} onPress={() => {}} />,
+        tabBarIcon: ({focused}) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = 'house-chimney';
+          } else if (route.name === 'Meal') {
+            iconName = 'utensils';
+          } else if (route.name === 'Shopping') {
+            iconName = 'basket-shopping';
+          } else if (route.name === 'Profile') {
+            iconName = 'user';
+          }
+
+          return <TabBarIcon iconName={iconName || ''} focused={focused} label = {route.name} />;
+        },
+        tabBarActiveTintColor: '#50C878',
+        tabBarInactiveTintColor: '#E4EDE7',
+        tabBarShowLabel: false,
+        headerShown: false,
+      })}>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Meal" component={Meal} />
+      <Tab.Screen name="Shopping" component={Shopping} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+};
+
+export default TabBar;
---
+++ src/screens/index.ts
@@ -0,0 +1,4 @@
+export {default as Home} from './Home';
+export {default as Profile} from './Profile';
+export {default as Meal} from './Meal';
+export {default as Shopping} from './Shopping';
